b10d8816ea62db1fc5bb0e46cf569630b67ec935
HenriquePRA <henriquep.r@hotmail.com>
2020-12-17 03:10:21 -0300

    skills 30%

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './scss/main.scss';
 
 import Fotoperfil from './img/misc/profilepic.jpg';
-import Github from './img/github.svg';
+import Github from './img/tools/github.svg';
 import Linkedin from './img/misc/linkedin.svg';
 import htmlSvg from './img/tools/html-5.svg';
 import css3Svg from './img/tools/css.svg';
@@ -19,14 +19,15 @@ import pythonSvg from './img/tools/python.svg';
 import postgreSvg from './img/tools/postgresql.svg';
 import javaSvg from './img/tools/java.svg';
 import phpSvg from './img/tools/php.svg';
-import rubySvg from './img/tools/ruby.svg';
-import railsSvg from './img/tools/rails.svg';
 import laravelSvg from './img/tools/laravel.svg';
 import firebaseSvg from './img/tools/firebase.svg';
 import nodeSvg from './img/tools/nodejs.svg'
 import djangoSvg from './img/tools/django.svg';
 import expressSvg from './img/tools/express.svg';
 import mongoSvg from './img/tools/mongodb.svg';
+import mysqlSvg from './img/tools/mysql.svg';
+import gitSvg from './img/tools/git.svg';
+import herokuSvg from './img/tools/heroku.svg';
 
 function Portfolio() {
   return (
@@ -68,159 +69,189 @@ function Portfolio() {
         <div className='Skills'>
             
             <div className="front">
-                <h3>Front-End</h3>
-                <div className="frontCardsContainer">
-                    <div className="frontCard">
-                        <img src={sassSvg} alt="Sass logo" />
-                        <div className="frontCardTextBg bg-sass">
-                            <span>Sass</span>
+                <h3>Linguagems de Programação</h3>
+                <div className="cardsContainer">
+
+                    <div className="Card">
+                        <img src={pythonSvg} alt="python logo" />
+                        <div className="CardTextBg bg-minicard">
+                            <span>Python</span>
                         </div>
                     </div>
 
-                    <div className="frontCard">
-                        <img src={reactSvg} alt="react logo"/>
-                        <div className="frontCardTextBg bg-react" >
-                            <span>React</span>
+                    <div className="Card">
+                        <img src={phpSvg} alt="php logo" />
+                        <div className="CardTextBg bg-minicard">
+                            <span>PHP</span>
                         </div>
                     </div>
 
-                    <div className="frontCard">
+                    <div className="Card">
+                        <img src={javaSvg} alt="java logo" />
+                        <div className="CardTextBg bg-minicard">
+                            <span>Java</span>
+                        </div>
+                    </div>
+
+                    <div className="Card">
                         <img src={javascriptSvg} alt="Javascript logo" />
-                        <div className="frontCardTextBg bg-js">
+                        <div className="CardTextBg bg-minicard">
                             <span>Javascript</span>
                         </div>
-                    </div>                   
-
-                    <div className="frontCard">
-                        <img src={htmlSvg} alt="Html logo" />
-                        <div className="frontCardTextBg bg-html">
-                            <span>HTML</span>
-                        </div>
                     </div>
 
-                    <div className="frontCard">
-                        <img src={css3Svg} alt="Css logo" />
-                        <div className="frontCardTextBg bg-css3">
-                            <span>CSS</span>
-                        </div>
-                    </div>
+                </div>
+            </div>
 
-                    <div className="frontCard">
-                        <img src={tailwindSvg} alt="Tailwind logo" />
-                        <div className="frontCardTextBg bg-tailwind">
-                            <span>TailwindCSS</span>
-                        </div>
-                    </div>
+            <div className="back">
+                <h3>Frameworks</h3>
+                <div className="cardsContainer">
 
-                    <div className="frontCard">
+                    <div className="Card">
                         <img src={angularSvg} alt="angular logo" />
-                        <div className="frontCardTextBg bg-angular">
+                        <div className="CardTextBg bg-minicard">
                             <span>Angular</span>
                         </div>
                     </div>
 
-                    <div className="frontCard">
+                    <div className="Card">
+                        <img src={htmlSvg} alt="Html logo" />
+                        <div className="CardTextBg bg-minicard">
+                            <span>HTML</span>
+                        </div>
+                    </div>
+
+                    <div className="Card">
+                        <img src={laravelSvg} alt="laravel logo" />
+                        <div className="CardTextBg  bg-minicard">
+                            <span>Laravel</span>
+                        </div>
+                    </div>
+
+                    <div className="Card">
+                        <img src={sassSvg} alt="Sass logo" />
+                        <div className="CardTextBg bg-minicard">
+                            <span>Sass</span>
+                        </div>
+                    </div>
+
+                    <div className="Card">
                         <img src={bootstrapSvg} alt="bootstrap logo" />
-                        <div className="frontCardTextBg bg-bootstrap">
+                        <div className="CardTextBg bg-minicard">
                             <span>Bootstrap</span>
                         </div>
                     </div>
 
-                    <div className="frontCard">
+                    <div className="Card">
+                        <img src={css3Svg} alt="Css logo" />
+                        <div className="CardTextBg bg-minicard">
+                            <span>CSS</span>
+                        </div>
+                    </div>
+
+                    <div className="Card">
+                        <img src={reactSvg} alt="react logo"/>
+                        <div className="CardTextBg bg-minicard" >
+                            <span>React</span>
+                        </div>
+                    </div>
+
+                    <div className="Card">
+                        <img src={tailwindSvg} alt="Tailwind logo" />
+                        <div className="CardTextBg bg-minicard">
+                            <span>Tailwind</span>
+                        </div>
+                    </div>
+
+                    <div className="Card">
+                        <img src={djangoSvg} alt="django logo" />
+                        <div className="CardTextBg  bg-minicard">
+                            <span>Django</span>
+                        </div>
+                    </div>
+
+                    <div className="Card">
                         <img src={vueSvg} alt="vue logo" />
-                        <div className="frontCardTextBg bg-vue-django">
+                        <div className="CardTextBg bg-minicard">
                             <span>Vue.js</span>
+                        </div>
+                    </div>
+
+                    <div className="Card">
+                        <img src={nodeSvg} alt="node logo" />
+                        <div className="CardTextBg  bg-minicard">
+                            <span>NodeJS</span>
+                        </div>
+                    </div>
+
+                    <div className="Card">
+                        <img src={expressSvg} alt="express logo" />
+                        <div className="CardTextBg  bg-minicard">
+                            <span>ExpressJS</span>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div className="back">
-                <h3>Back-End</h3>
-                <div className="backcardsContainer">
-                    <div className="backCard">
-                        <img src={pythonSvg} alt="python logo" />
-                        <div className="backCardTxtbg bg-postgres-python">
-                            <span>Python</span>
+                <h3>Bancos de dados</h3>
+                <div className="cardsContainer">
+
+                    <div className="Card">
+                        <img src={mysqlSvg} alt="mysql logo" />
+                        <div className="CardTextBg  bg-minicard">
+                            <span>Mysql</span>
                         </div>
                     </div>
 
-                    <div className="backCard">
-                        <img src={javaSvg} alt="java logo" />
-                        <div className="backCardTxtbg bg-java">
-                            <span>Java</span>
+                    <div className="Card">
+                        <img src={postgreSvg} alt="postgresql logo" />
+                        <div className="CardTextBg  bg-minicard">
+                            <span>PostgreSQL</span>
                         </div>
                     </div>
 
-                    <div className="backCard">
-                        <img src={phpSvg} alt="php logo" />
-                        <div className="backCardTxtbg bg-php">
-                            <span>PHP</span>
+                    <div className="Card">
+                        <img src={mongoSvg} alt="mongo logo" />
+                        <div className="CardTextBg  bg-minicard">
+                            <span>MongoDB</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="back">
+                <h3>Ferramentas</h3>
+                <div className="cardsContainer">
+
+                    <div className="Card">
+                        <img src={Github} alt="github logo" />
+                        <div className="CardTextBg bg-minicard">
+                            <span>Github</span>
                         </div>
                     </div>
 
-                    <div className="backCard">
-                        <img src={rubySvg} alt="ruby logo" />
-                        <div className="backCardTxtbg bg-ruby">
-                            <span>Ruby</span>
+                    <div className="Card">
+                        <img src={gitSvg} alt="git logo" />
+                        <div className="CardTextBg bg-minicard">
+                            <span>Git</span>
                         </div>
                     </div>
 
-                    <div className="backCard">
-                        <img src={railsSvg} style={{'width':'7rem'}} alt="rails logo" />
-                        <div className="backCardTxtbg bg-ruby">
-                            <span>Ruby on Rails</span>
-                        </div>
-                    </div>
-
-                    <div className="backCard">
-                        <img src={laravelSvg} alt="laravel logo" />
-                        <div className="backCardTxtbg bg-laravel">
-                            <span>Laravel</span>
-                        </div>
-                    </div>
-
-                    <div className="backCard">
+                    <div className="Card">
                         <img src={firebaseSvg} alt="firebase logo" />
-                        <div className="backCardTxtbg bg-firebase">
+                        <div className="CardTextBg  bg-minicard">
                             <span>Firebase</span>
                         </div>
                     </div>
 
-                    <div className="backCard">
-                        <img src={nodeSvg} alt="node logo" />
-                        <div className="backCardTxtbg bg-node-mongo">
-                            <span>NodeJS</span>
+                    <div className="Card">
+                        <img src={herokuSvg} alt="heroku logo" />
+                        <div className="CardTextBg bg-minicard">
+                            <span>Heroku</span>
                         </div>
                     </div>
 
-                    <div className="backCard">
-                        <img src={djangoSvg} alt="django logo" />
-                        <div className="backCardTxtbg bg-vue-django">
-                            <span>Django</span>
-                        </div>
-                    </div>
-
-                    <div className="backCard">
-                        <img src={expressSvg} alt="express logo" />
-                        <div className="backCardTxtbg bg-express">
-                            <span>ExpressJS</span>
-                        </div>
-                    </div>
-
-                    <div className="backCard">
-                        <img src={mongoSvg} alt="mongo logo" />
-                        <div className="backCardTxtbg bg-node-mongo">
-                            <span>MongoDB</span>
-                        </div>
-                    </div>
-
-                    <div className="backCard">
-                        <img src={postgreSvg} alt="postgresql logo" />
-                        <div className="backCardTxtbg bg-postgres-python">
-                            <span>PostgreSQL</span>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
