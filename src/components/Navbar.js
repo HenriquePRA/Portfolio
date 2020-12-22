d2549800502c5614570f935daf1f475b18888e3c
HenriquePRA <henriquep.r@hotmail.com>
2020-12-22 15:17:56 -0300

    navbar statica ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import sobreSvg from '../img/misc/sobre.svg'
+import projetosSvg from '../img/misc/projects.svg'
+import contatoSvg from '../img/misc/contato.svg'
+
 
 const Navbar = () => {
     return (
         <div id="navbar">
             <div className="navContainer">
-                <button>SOBRE</button>
-                <button>PROJETOS</button>
-                <button>CONTATO</button>
+                <button>
+                    <img src={sobreSvg} alt="about icon"></img>
+                    <span>SOBRE</span>
+                </button>
+                <button>
+                    <img src={projetosSvg} alt="projects icon"></img>
+                    <span>PROJETOS</span>                    
+                </button>
+                <button>
+                    <img src={contatoSvg} alt="contact icon"></img>
+                    <span>CONTATO</span>
+                </button>
             </div>
         </div>
     )
