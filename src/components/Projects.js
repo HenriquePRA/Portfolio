7ecb22843761231b93af05cf9331b246e6d0ee11
HenriquePRA <henriquep.r@hotmail.com>
2020-12-28 21:06:24 -0300

    adição de um projeto a lista de projetos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -38,70 +38,78 @@ const Projects = (props) => {
 
     // array com todos os projetos
     const projetos = [
-        <div className="proj" key="prj9">
+        <div className="proj" key="prj10">
             <h4>Must-Do List</h4>
             <p>
                 Esse é meu primeiro desenvolvendo apps para plataformas android, é um
                 to do list com dados persistentes.
             </p>
-            <img src={androidSvg} className="langicon one_of4_stk" alt="Android Icon"></img>
-            <img src={vueSvg} className="langicon two_of4_stk" alt="Vue Icon"></img>
-            <img src={springSvg} className="langicon three_of4_stk" alt="Spring Icon"></img>
-            <img src={mongoSvg} className="langicon four_of4_stk" alt="Mongo Icon"></img>
+            <img src={androidSvg} className="langicon one_of4_stk" alt="Android"></img>
+            <img src={vueSvg} className="langicon two_of4_stk" alt="Vue"></img>
+            <img src={springSvg} className="langicon three_of4_stk" alt="Spring"></img>
+            <img src={mongoSvg} className="langicon four_of4_stk" alt="Mongo"></img>
 
             <div className="projButton" onClick={() => updateProj("Must-Do List")}>
                 <span>VER MAIS</span>
             </div>
         </div>,
-        <div className="proj" key="prj8">
+        <div className="proj" key="prj9">
             <h4>IF Mobile</h4>
             <p>
                 Projeto final de bancos de dados relacionais da disciplina de banco de dados 2
             </p>
-            <img src={postgreSvg} className="langicon one_of5_stk" alt="Postgres Icon"></img>
-            <img src={expressSvg} className="langicon two_of5_stk" alt="express Icon"></img>
-            <img src={reactSvg} className="langicon three_of5_stk" alt="react Icon"></img>
-            <img src={nodeSvg} className="langicon four_of5_stk" alt="Node Icon"></img>
-            <img src={bootstrapSvg} className="langicon five_of5_stk" alt="bootstrap Icon"></img>     
+            <img src={postgreSvg} className="langicon one_of5_stk" alt="Postgres"></img>
+            <img src={expressSvg} className="langicon two_of5_stk" alt="express"></img>
+            <img src={reactSvg} className="langicon three_of5_stk" alt="react"></img>
+            <img src={nodeSvg} className="langicon four_of5_stk" alt="Node"></img>
+            <img src={bootstrapSvg} className="langicon five_of5_stk" alt="bootstrap"></img>     
 
             <div className="projButton" onClick={() => updateProj("IF Mobile")}>
                 <span>VER MAIS</span>
             </div>
         </div>,
-        <div className="proj" key="prj7">
+        <div className="proj" key="prj8">
             <h4>Vendor Pop</h4>
             <p>Projeto final da disciplina de programação orientada a objetos, esse projeto trata-se app de vendas. 
             </p>
-            <img src={angularSvg} className="langicon one_of3_stk" alt="Spring Icon"></img>
-            <img src={springSvg} className="langicon two_of3_stk" alt="Spring Icon"></img>
-            <img src={mysqlSvg} className="langicon three_of3_stk" alt="mysql Icon"></img>
+            <img src={angularSvg} className="langicon one_of3_stk" alt="Spring"></img>
+            <img src={springSvg} className="langicon two_of3_stk" alt="Spring"></img>
+            <img src={mysqlSvg} className="langicon three_of3_stk" alt="mysql"></img>
 
             <div className="projButton" onClick={() => updateProj("Vendor Pop")}>
                 <span>VER MAIS</span>
             </div>
         </div>,
-        <div className="proj" key="prj6">
+        <div className="proj" key="prj7">
             <h4>Swing Memory Game</h4>
             <p>Esse projeto consiste em dois jogos de Memória onde um faz o uso do java swing e outro faz o console.</p>
-            <img src={javaSvg} className="langicon one_stk" alt="Java Icon"></img>
+            <img src={javaSvg} className="langicon one_stk" alt="Java"></img>
             <div className="projButton" onClick={() => updateProj("Swing Memory Game")}>
                 <span>VER MAIS</span>
             </div>
         </div>,
-        <div className="proj" key="prj5">
+        <div className="proj" key="prj6">
             <h4>React/Sass Portfolio</h4>
             <p>Criei esse portfolio como um projeto a fim de dominar o react com sass e exibir meus projetos.</p>
-            <img src={reactSvg} className="langicon one_of2_stk" alt="React Icon"></img>
-            <img src={sassSvg} className="langicon two_of2_stk" alt="Sass Icon"></img>                
+            <img src={reactSvg} className="langicon one_of2_stk" alt="React"></img>
+            <img src={sassSvg} className="langicon two_of2_stk" alt="Sass"></img>                
             <div className="projButton" onClick={() => updateProj("React/Sass Portfolio")}>
+                <span>VER MAIS</span>
+            </div>
+        </div>,
+        <div className="proj" key="prj5">
+            <h4>Modelagem de Banco de Dados</h4>
+            <p>Feito na disciplina de banco de dados 1 neste projeto eu modelo um banco de dados relacional.</p>
+            <img src={mysqlSvg} className="langicon one_stk" alt="MySql"></img>
+            <div className="projButton" onClick={() => updateProj("Modelagem de Banco de Dados")}>
                 <span>VER MAIS</span>
             </div>
         </div>,
         <div className="proj" key="prj4">
             <h4>Engine de busca</h4>
             <p>Projeto final de estruturas de dados esse projeto resume-se em uma engine de busca.</p>
-            <img src={djangoSvg} className="langicon one_of2_stk" alt="Django Icon"></img>
-            <img src={bootstrapSvg} className="langicon two_of2_stk" alt="Bootstrap Icon"></img>                
+            <img src={djangoSvg} className="langicon one_of2_stk" alt="Django"></img>
+            <img src={bootstrapSvg} className="langicon two_of2_stk" alt="Bootstrap"></img>                
             <div className="projButton" onClick={() => updateProj("Engine de busca (Framework de detecção Sinonímia)")}>
                 <span>VER MAIS</span>
             </div>
@@ -109,10 +117,10 @@ const Projects = (props) => {
         <div className="proj" key="prj3">
             <h4>My Book List</h4>
             <p>Usando o servico firebase e uma api de livros esse projeto faz o crud de usuarios e livros.</p>
-            <img src={HtmlSvg} className="langicon one_of4_stk" alt="HTML Icon"></img>
-            <img src={CssSvg} className="langicon two_of4_stk" alt="CSS Icon"></img>
-            <img src={JavascriptSvg} className="langicon three_of4_stk" alt="Javascript Icon"></img>
-            <img src={firebaseSvg} className="langicon four_of4_stk" alt="Firebase Icon"></img>
+            <img src={HtmlSvg} className="langicon one_of4_stk" alt="HTML"></img>
+            <img src={CssSvg} className="langicon two_of4_stk" alt="CSS"></img>
+            <img src={JavascriptSvg} className="langicon three_of4_stk" alt="Javascript"></img>
+            <img src={firebaseSvg} className="langicon four_of4_stk" alt="Firebase"></img>
             <div className="projButton" onClick={() => updateProj("My Book List")}>
                 <span>VER MAIS</span>
             </div>
@@ -120,7 +128,7 @@ const Projects = (props) => {
         <div className="proj" key="prj2">
             <h4>Naval Battle</h4>
             <p>Esse projeto simula um jogo de batalha naval foi minha primeira experiencia com python.</p>
-            <img src={pythonSvg} className="langicon one_stk" alt="HTML Icon"></img>
+            <img src={pythonSvg} className="langicon one_stk" alt="HTML"></img>
             <div className="projButton" onClick={() => updateProj("Naval Battle")}>
                 <span>VER MAIS</span>
             </div>
@@ -128,8 +136,8 @@ const Projects = (props) => {
         <div className="proj" key="prj1">
             <h4>Byte Barrage</h4>
             <p>Esse é meu primeiro projeto front-end, simula um site catalogo de venda de propriedades em jogos.</p>
-            <img src={HtmlSvg} className="langicon one_of2_stk" alt="HTML Icon"></img>
-            <img src={CssSvg} className="langicon two_of2_stk" alt="CSS Icon"></img>
+            <img src={HtmlSvg} className="langicon one_of2_stk" alt="HTML"></img>
+            <img src={CssSvg} className="langicon two_of2_stk" alt="CSS"></img>
             <div className="projButton" onClick={() => updateProj("Byte Barrage")}>
                 <span>VER MAIS</span>
             </div>
