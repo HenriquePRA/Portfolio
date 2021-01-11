e292854c807ae0c19c97c84d0aa171a7de8889fb
HenriquePRA <henriquep.r@hotmail.com>
2021-01-11 00:04:41 -0300

    Atualização de Responsividade em  Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -46,10 +46,10 @@ const Projects = (props) => {
                 Esse é meu primeiro desenvolvendo apps para plataformas android, é um
                 to do list com dados persistentes.
             </p>
-            <img src={androidSvg} className="langicon one_of4_stk" alt="Android"></img>
-            <img src={vueSvg} className="langicon two_of4_stk" alt="Vue"></img>
-            <img src={springSvg} className="langicon three_of4_stk" alt="Spring"></img>
-            <img src={mongoSvg} className="langicon four_of4_stk" alt="Mongo"></img>
+            <img src={androidSvg} className="langicon col_1-3 row_4-5" alt="Android"></img>
+            <img src={vueSvg} className="langicon col_2-4 row_4-5" alt="Vue"></img>
+            <img src={springSvg} className="langicon col_1-3 row_5-6" alt="Spring"></img>
+            <img src={mongoSvg} className="langicon col_2-4 row_5-6" alt="Mongo"></img>
 
             <div className="projButton" onClick={() => updateProj("Must-Do List")}>
                 <span>VER MAIS</span>
@@ -60,11 +60,11 @@ const Projects = (props) => {
             <p>
                 Projeto final de bancos de dados relacionais da disciplina de banco de dados 2
             </p>
-            <img src={postgreSvg} className="langicon one_of5_stk" alt="Postgres"></img>
-            <img src={expressSvg} className="langicon two_of5_stk" alt="express"></img>
-            <img src={reactSvg} className="langicon three_of5_stk" alt="react"></img>
-            <img src={nodeSvg} className="langicon four_of5_stk" alt="Node"></img>
-            <img src={bootstrapSvg} className="langicon five_of5_stk" alt="bootstrap"></img>     
+            <img src={postgreSvg} className="langicon col_1-3 row_4-5" alt="Postgres"></img>
+            <img src={expressSvg} className="langicon col_2-4 row_4-5" alt="express"></img>
+            <img src={reactSvg} className="langicon col_3-5 row_4-5" alt="react"></img>
+            <img src={nodeSvg} className="langicon col_2-3 row_5-6" alt="Node"></img>
+            <img src={bootstrapSvg} className="langicon col_3-4 row_5-6" alt="bootstrap"></img>     
 
             <div className="projButton" onClick={() => updateProj("IF Mobile")}>
                 <span>VER MAIS</span>
@@ -74,9 +74,9 @@ const Projects = (props) => {
             <h4>Vendor Pop</h4>
             <p>Projeto final da disciplina de programação orientada a objetos, esse projeto trata-se app de vendas. 
             </p>
-            <img src={angularSvg} className="langicon one_of3_stk" alt="Spring"></img>
-            <img src={springSvg} className="langicon two_of3_stk" alt="Spring"></img>
-            <img src={mysqlSvg} className="langicon three_of3_stk" alt="mysql"></img>
+            <img src={angularSvg} className="langicon col_1-3 row_4-6" alt="Spring"></img>
+            <img src={springSvg} className="langicon col_2-4 row_4-6" alt="Spring"></img>
+            <img src={mysqlSvg} className="langicon col_3-5 row_4-6" alt="mysql"></img>
 
             <div className="projButton" onClick={() => updateProj("Vendor Pop")}>
                 <span>VER MAIS</span>
@@ -85,7 +85,7 @@ const Projects = (props) => {
         <div className="proj" key="prj7">
             <h4>Swing Memory Game</h4>
             <p>Esse projeto consiste em dois jogos de Memória onde um faz o uso do java swing e outro faz o console.</p>
-            <img src={javaSvg} className="langicon one_stk" alt="Java"></img>
+            <img src={javaSvg} className="langicon col_1-3 row_4-6" alt="Java"></img>
             <div className="projButton" onClick={() => updateProj("Swing Memory Game")}>
                 <span>VER MAIS</span>
             </div>
@@ -93,8 +93,8 @@ const Projects = (props) => {
         <div className="proj" key="prj6">
             <h4>React/Sass Portfolio</h4>
             <p>Criei esse portfolio como um projeto a fim de dominar o react com sass e exibir meus projetos.</p>
-            <img src={reactSvg} className="langicon one_of2_stk" alt="React"></img>
-            <img src={sassSvg} className="langicon two_of2_stk" alt="Sass"></img>                
+            <img src={reactSvg} className="langicon col_1-3 row_4-6" alt="React"></img>
+            <img src={sassSvg} className="langicon col_2-4 row_4-6" alt="Sass"></img>                
             <div className="projButton" onClick={() => updateProj("React/Sass Portfolio")}>
                 <span>VER MAIS</span>
             </div>
@@ -102,7 +102,7 @@ const Projects = (props) => {
         <div className="proj" key="prj5">
             <h4>Modelagem de Banco de Dados</h4>
             <p>Feito na disciplina de banco de dados 1 neste projeto eu modelo um banco de dados relacional.</p>
-            <img src={mysqlSvg} className="langicon one_stk" alt="MySql"></img>
+            <img src={mysqlSvg} className="langicon col_1-3 row_4-6" alt="MySql"></img>
             <div className="projButton" onClick={() => updateProj("Modelagem de Banco de Dados")}>
                 <span>VER MAIS</span>
             </div>
@@ -110,8 +110,8 @@ const Projects = (props) => {
         <div className="proj" key="prj4">
             <h4>Engine de busca</h4>
             <p>Projeto final de estruturas de dados esse projeto resume-se em uma engine de busca.</p>
-            <img src={djangoSvg} className="langicon one_of2_stk" alt="Django"></img>
-            <img src={bootstrapSvg} className="langicon two_of2_stk" alt="Bootstrap"></img>                
+            <img src={djangoSvg} className="langicon col_1-3 row_4-6" alt="Django"></img>
+            <img src={bootstrapSvg} className="langicon col_2-4 row_4-6" alt="Bootstrap"></img>                
             <div className="projButton" onClick={() => updateProj("Engine de busca (Framework de detecção Sinonímia)")}>
                 <span>VER MAIS</span>
             </div>
@@ -119,16 +119,12 @@ const Projects = (props) => {
         <div className="proj" key="prj3">
             <h4>Visar Livros</h4>
             <p>Projeto final da disciplina de linguagens de script, com suporte a navegadores antigos trata-se de uma página de busca integrada com a API de livros do Google.</p>
-            <img src={HtmlSvg} className="langicon one_of6_stk" alt="HTML"></img>
-            <img src={CssSvg} className="langicon two_of6_stk" alt="CSS"></img>
-            <img src={bootstrapSvg} className="langicon three_of6_stk" alt="Bootstrap"></img>
-            <img src={JavascriptSvg} className="langicon four_of6_stk" alt="Javascript"></img>
-            <img src={webpackSvg} className="langicon five_of6_stk" alt="Webpack"></img>            
-            <img src={babelSvg} 
-                className="langicon six_of6_stk" 
-                style={{"height": "auto", "width": "4rem", "justifySelf":"right"}}
-                alt="Babel"
-            ></img>
+            <img src={HtmlSvg} className="langicon col_1-3 row_4-5" alt="HTML"></img>
+            <img src={CssSvg} className="langicon col_2-4 row_4-5" alt="CSS"></img>
+            <img src={bootstrapSvg} className="langicon col_3-5 row_4-5" alt="Bootstrap"></img>
+            <img src={JavascriptSvg} className="langicon col_1-3 row_5-6" alt="Javascript"></img>
+            <img src={webpackSvg} className="langicon col_2-4 row_5-6" alt="Webpack"></img>            
+            <img src={babelSvg} className="langicon col_3-5 row_5-6 long_stk" alt="Babel"></img>
             <div className="projButton" onClick={() => updateProj("Visar Livros")}>
                 <span>VER MAIS</span>
             </div>
@@ -136,7 +132,7 @@ const Projects = (props) => {
         <div className="proj" key="prj2">
             <h4>Naval Battle</h4>
             <p>Feito em python trata-se de um jogo de batalha naval que pode ser jogado entre jogadores ou contra o computador</p>
-            <img src={pythonSvg} className="langicon one_stk" alt="HTML"></img>
+            <img src={pythonSvg} className="langicon col_1-3 row_4-6" alt="HTML"></img>
             <div className="projButton" onClick={() => updateProj("Naval Battle")}>
                 <span>VER MAIS</span>
             </div>
@@ -144,8 +140,8 @@ const Projects = (props) => {
         <div className="proj" key="prj1">
             <h4>Byte Barrage</h4>
             <p>Esse é meu primeiro projeto front-end, simula um site catalogo de venda de propriedades em jogos.</p>
-            <img src={HtmlSvg} className="langicon one_of2_stk" alt="HTML"></img>
-            <img src={CssSvg} className="langicon two_of2_stk" alt="CSS"></img>
+            <img src={HtmlSvg} className="langicon col_1-3 row_4-6" alt="HTML"></img>
+            <img src={CssSvg} className="langicon col_2-4 row_4-6" alt="CSS"></img>
             <div className="projButton" onClick={() => updateProj("Byte Barrage")}>
                 <span>VER MAIS</span>
             </div>
